cb7f6a664b7cfbf68b9481ab0281f3bd827d1a44
jerrykim91 <sun4131@gmail.com>
2021-02-25 01:37:55 +0900

    Blog2__2021.02.25 [ History ] update-1

--- project/blog/views.py
+++ project/blog/views.py
@@ -11,8 +11,8 @@ from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthA
 from django.views.generic.dates import DayArchiveView, TodayArchiveView
 
 # 2-1. 테이블 조회를 위한 모델 임포트
-from blog.models import Post, Category, PhotoArt
-
+from blog.models import Post, Category
+# from blog.models import Post, Category, PhotoArt
 # 2-2. 템플릿 뷰
 from django.views.generic import TemplateView
 
